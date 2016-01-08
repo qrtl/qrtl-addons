af7fb890c6a4fff15c75b4ca5b9691c3ebfc03ea
Yoshi Tashiro <tashiro@roomsfor.hk>
2016-01-08 19:25:07 +0800

    [ADD] add modules

---
+++ stock_picking_menu/__openerp__.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+#    Odoo, Open Source Management Solution
+#    Copyright (C) 2015 Rooms For (Hong Kong) Limited T/A OSCG
+#    <https://www.odoo-asia.com>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+{
+    'name': 'Stock Picking Menu',
+    'version': '8.0.0.6.0',
+    'category': 'Menu',
+    'description': """
+* Adds menu items for users to reach picking operation screens quickly.
+* Default proposal of Picking Type is for the first warehouse.
+    """,
+    'author': 'Rooms For (Hong Kong) Limited T/A OSCG',
+    'website': 'https://www.odoo-asia.com',
+    'license': 'AGPL-3',
+    'depends': ["stock"],
+    'data': [
+        'stock_view.xml',
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False, 
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
