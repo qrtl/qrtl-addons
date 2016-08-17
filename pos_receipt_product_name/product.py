c5fb5494d38b4a9c646c27eb0947c0a944a8e1a4
Yoshi Tashiro <tashiro@roomsfor.hk>
2016-08-17 11:01:13 -0400

    [IMP] flake8 for all modules 2

--- pos_receipt_product_name/product.py
+++ pos_receipt_product_name/product.py
@@ -7,7 +7,6 @@
 #    it under the terms of the GNU Affero General Public License as
 #    published by the Free Software Foundation, either version 3 of the
 #    License, or (at your option) any later version.
-#
 #    This program is distributed in the hope that it will be useful,
 #    but WITHOUT ANY WARRANTY; without even the implied warranty of
 #    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
@@ -17,6 +16,7 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 from openerp.osv import fields, osv
+
 
 class product_template(osv.osv):
     _inherit = "product.template"
