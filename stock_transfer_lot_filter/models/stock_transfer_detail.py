d4098f9f0a22cbabca14f460b70101012f9ddfce
Yoshi Tashiro <tashiro@roomsfor.hk>
2017-01-24 18:04:12 +0800

    [FIX] bad line break

--- stock_transfer_lot_filter/models/stock_transfer_detail.py
+++ stock_transfer_lot_filter/models/stock_transfer_detail.py
@@ -1,20 +1,6 @@
 # -*- coding: utf-8 -*-
-#    Odoo, Open Source Management Solution
-#    Copyright (C) 2015-2016 Rooms For (Hong Kong) Limited T/A OSCG
-#    <https://www.odoo-asia.com>
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# Copyright 2015-2017 Rooms For (Hong Kong) Limited T/A OSCG
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from openerp import models, api, _
 
@@ -38,8 +24,9 @@ class stock_transfer_details(models.TransientModel):
         if code == 'incoming':
             view = self.env.ref('stock.view_stock_enter_transfer_details')
         else:
-            view = self.env.ref('stock_transfer_lot_filter.\
-                    view_stock_enter_transfer_details_z1')
+            view = self.env.ref(
+                'stock_transfer_lot_filter.'
+                'view_stock_enter_transfer_details_z1')
 
         return {
             'name': _('Enter transfer details'),
