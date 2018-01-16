ae001cf98430d2b5e7c4f468aed0b6b8e5ac64cc
tslai <tl@quartile.co>
2018-01-16 09:07:50 +0000

    [IMP] Support the address input on  e-commerce

--- l10n_jp_address_layout/__manifest__.py
+++ l10n_jp_address_layout/__manifest__.py
@@ -3,20 +3,22 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     'name': 'Japan Address Layout',
-    'version': '11.0.1.0.0',
+    'version': '11.0.2.0.0',
     'author': 'Quartile Limited',
     'website': 'https://www.odoo-asia.com',
     'category': 'Extra Tools',
     'license': "AGPL-3",
     'description': """
-This module provides the Japan address input field layout.
+This module provides the Japan address input field layout in both backend
+and frontend.
     """,
     'summary': "",
     'depends': [
-        'base',
+        'website_sale',
     ],
     'data': [
         'views/res_partner_views.xml',
+        'views/templates.xml',
         'data/res_country_data.xml',
     ],
     'installable': True,
